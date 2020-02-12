b8795a0b30e7a804e0253462702d3ab396c4d945
mylonturner <mylonturner@gmail.com>
2020-02-12 02:26:03 +0000

    added songs and artist loops

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var songName = ["collide", "maybe","heaven on earth"];
+songName.forEach(function(element){
+ $(".songName").append("<p> " + element + " </p>");
+});
+
+var artist = ["justine skye", "justine skye","yo gotti"];
+artist.forEach(function(element){
+ $(".artist").append("<p> " + element + " </p>");
+});
+
