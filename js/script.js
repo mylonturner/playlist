f55cef84e71778c06dd2ff36d8524602ab8278a9
mylonturner <mylonturner@gmail.com>
2020-02-19 02:29:44 +0000

    yaay

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-var songName = ["collide", "maybe","heaven on earth"];
+var songName = ["collide", "maybe","rehab"];
 songName.forEach(function(element){
  $(".songName").append("<p> " + element + " </p>");
 });
 
-var artist = ["justine skye", "justine skye","yo gotti"];
+var artist = ["justine skye", "justine skye","brent faiyaz"];
 artist.forEach(function(element){
  $(".artist").append("<p> " + element + " </p>");
 });
+
+var songLength = ["4 mins", "2 mins","3 mins"];
+songLength.forEach(function(element){
+ $(".songLength").append("<p> " + element + " </p>");
+});
+
+var songLength = ["4 mins", "2 mins","3 mins"];
+songLength.forEach(function(element){
+ $(".songLength").append("<p> " + element + " </p>");
+});
+
+var link = ["www.youtube.com › watch","www.youtube.com › watch","www.youtube.com › watch"];
+link.forEach(function(element){
+ $(".link").append("<p> " + element + " </p>");
+});
+
 
